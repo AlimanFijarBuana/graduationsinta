be2dffd8e6d1b67101843bc74901ee4b26846b5c
ALIMAN FIJAR BUANA <154599892+AlimanFijarBuana@users.noreply.github.com>
2025-08-18 00:36:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -633,29 +633,20 @@ def main():
 
     components.html(html_code, height=500)
 
-    col1, col2, col3 = st.columns([1,2,1])
-
-    with col2:
-        if st.button("👑 Click for Royal Celebration!", key="celebrate_btn"):
-            fire_confetti()
-            st.markdown("""
-                <div style="
-                    display: flex;
-                    justify-content: center;
-                    text-align: center;
-                    margin: 30px 0;
-                ">
-                    <div style="
-                        font-family: 'Playfair Display', serif;
-                        color: #d23669;
-                        font-size: 1.3rem;
-                    ">
-                        Semoga setiap langkahmu selalu dipenuhi cahaya,<br>
-                        karena kamu pantas bersinar lebih dari siapapun! ✨
-                    </div>
-                </div>
-            """, unsafe_allow_html=True)
-
+    if st.button("👑 Click for Royal Celebration!", key="celebrate_btn"):
+        fire_confetti()
+        st.markdown("""
+        <div style="
+            font-family: 'Playfair Display', serif;
+            color: #d23669;
+            font-size: 1.3rem;
+            text-align: center;
+            margin: 30px 0;
+        ">
+            Semoga setiap langkahmu selalu dipenuhi cahaya, karena kamu pantas bersinar lebih dari siapapun! ✨
+        </div>
+        """, unsafe_allow_html=True)
+    
     st.markdown(f"""
     <div class="footer">
         Made with 💖 for Princess {data['name']}'s {data['graduation_year']} Graduation
